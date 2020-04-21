72d6400e0b167468b210498416c8e1b18b71be2c
migueljr84 <mig.lima.jr.84@gmail.com>
2020-04-21 20:08:29 -0300

    Arquivo comitado com sucesso

--- PreProcessamento/05-Caret.R
+++ PreProcessamento/05-Caret.R
@@ -1,12 +1,6 @@
-# Usando o Pacote Caret Para Criar Modelos de Machine Learning em R
-# http://topepo.github.io/caret/index.html
 
-# Obs: Caso tenha problemas com a acentuação, consulte este link:
-# https://support.rstudio.com/hc/en-us/articles/200532197-Character-Encoding
 
-# Configurando o diretório de trabalho
-# Coloque entre aspas o diretório de trabalho que você está usando no seu computador
-# Não use diretórios com espaço no nome
+
 setwd("C:/Temp/Python/01-BigDataAnalyticsComR_Azure_MachineLearning/cap12-IntroducaoAzureMachineLearning")
 getwd()
 
